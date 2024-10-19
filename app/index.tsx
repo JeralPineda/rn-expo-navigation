ca2396db68ea3a73326e63018513e6c14b5b851a
Jeral Pineda <jeral.cris@gmail.com>
2024-10-19 08:50:17 -0600

    fix: fuentes personalizadas en nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,16 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Hello World</Text>
-    </View>
+    <SafeAreaView>
+      <View className="mt-6 mx-2.5">
+        <Text className="text-3xl" style={{ fontFamily: "WorkSans-Black" }}>
+          Hello World
+        </Text>
+
+        <Text className="text-2xl font-work-black">Hola mundo</Text>
+        <Text className="text-xl font-work-light">Hola mundo</Text>
+      </View>
+    </SafeAreaView>
   );
 }
