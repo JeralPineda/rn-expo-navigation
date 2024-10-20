783db0c1ea51689e13904618737a94833645e528
Jeral Pineda <jeral.cris@gmail.com>
2024-10-20 17:02:19 -0600

    fix: personalizacion del StackNavigation

--- app/(stack)/_layout.tsx
+++ app/(stack)/_layout.tsx
@@ -2,7 +2,15 @@ import { Stack } from "expo-router";
 
 export default function StackLayout() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        // headerShown: false,
+        headerShadowVisible: false,
+        contentStyle: {
+          backgroundColor: "#fff",
+        },
+      }}
+    >
       <Stack.Screen name="home/index" options={{ title: "Home" }} />
       <Stack.Screen name="products/index" options={{ title: "Productos" }} />
       <Stack.Screen name="profile/index" options={{ title: "Perfil" }} />
